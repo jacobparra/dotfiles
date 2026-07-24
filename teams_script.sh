df04ea571178195e397c3e42070d280c09b6b484
Jacob Parra <jacobrpg@gmail.com>
2026-07-23 21:48:57 -0400

    Add GitHub and Gist credential helpers to gitconfig, include zsh-better-npm-completion plugin, update iTerm2 AI settings, and create Teams status refresh script

---
+++ teams_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+while true;
+do
+    osascript -e 'tell application "Microsoft Teams" to activate'
+    osascript -e 'tell application "System Events" to keystroke "2" using {command down}'
+    echo "Teams Status Refreshed"
+    sleep 300
+done
